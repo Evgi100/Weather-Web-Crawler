4f9d28b7988a87b7aee364f8ce8ce0c7a8da0797
Lana Maidenbaum <lanamaidenbaum@Lanas-MacBook-Pro.local>
2017-09-14 21:50:21 +0300

    first commit

---
+++ public/script.js
@@ -0,0 +1,98 @@
+var currentCity;
+var userCities=[];
+var currentDate = new Date();
+
+function grabDate(){
+     
+}
+
+
+// {{temp.temp-273.15}} °C / {{1.8 x (temp.temp-273.15) + 32}}
+function fetch(currentCity) {
+    $.get({
+        url: 'http://api.openweathermap.org/data/2.5/weather?q=' + currentCity + '&APPID=2c7ca9a42dca75e244be247891827ee1',
+        success: function (data) {
+            //update the userData using API data
+            console.log(data);
+            var city={
+                name:data.name,
+                temp: data.main,
+                currentCel:Math.round(data.main.temp-273.15),
+                currentFar:Math.round((data.main.temp-273.15)*1.8+32),
+                minTempC:Math.round(data.main.temp_min-273.15),
+                maxTempC:Math.round(data.main.temp_max-273.15),
+                minTempF:Math.round((data.main.temp_min-273.15)*1.8+32),
+                maxTempF:Math.round((data.main.temp_max-273.15)*1.8+32),
+                description:data.weather,
+                sys:data.sys,  
+                comments:[], 
+                date:currentDate.getDate()
+            }
+            userCities.push(city);
+            _renderCityTemps(userCities);
+
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log(textStatus);
+        }
+    });
+}
+
+function grabUserData() {
+    var cityName = $("#userInput").val();
+    $("#userInput").val('');
+    currentCity=cityName;
+}
+
+function _renderCityTemps(userCities){
+    $('.postCities').empty();
+    var source = $('#store-template').html();
+    var template = Handlebars.compile(source);
+    
+    userCities.forEach(function(city){
+        var newHTML = template(city);
+        $('.postCities').append(newHTML);
+    });
+}
+
+function createComment(postIndex, newComment) {
+    if (newComment) {
+        userCities[postIndex].comments.push(newComment);
+        renderComments(postIndex);
+    }
+    else {
+        alert('theres no text in yours comment');
+    }
+}
+
+function renderComments(postIndex) {
+    var $comments = $('.comments');
+    $comments.empty();
+
+    userCities[postIndex].comments.forEach(function (comment) {
+        $comments.append('<div class = "row"><button type="button" class="btn btn-circle">\
+    </button><div class="postComment col-lg-6">' + comment + '\
+    </div><button class="btn btn-xs btn-danger col-lg-6 glyphicon glyphicon-trash deleteComment"></button></div>');
+
+    });
+
+}
+
+$('.postCities').on('click','.commentButton', function(){
+   var commentText = $(this).offsetParent().siblings('input').val();
+   $(this).offsetParent().siblings('input').val('');
+
+    var postIndex = $('.commentButton').index($(this));
+
+    createComment(postIndex, commentText);
+});
+function _renderComments(){
+}
+
+$("#search").on('click', function () {
+    grabUserData();
+    fetch(currentCity);
+})
+
+var userinput = document.getElementById("userInput");
+var autocomplete = new google.maps.places.Autocomplete(userinput);
