ed5004415e599018a5c7842879694554d8624a7f
Evgi100 <evgeni.shapira@gmail.com>
2018-01-18 10:58:55 +0200

    Finished most functionality and add some UI/Front End

--- public/script.js
+++ public/script.js
@@ -1,10 +1,7 @@
 var currentCity;
 var userCities=[];
-var currentDate = new Date();
 
-function grabDate(){
-     
-}
+
 
 function convert(data){
    return Math.round(data-273.15)
@@ -12,26 +9,14 @@ function convert(data){
 
 // {{temp.temp-273.15}} °C / {{1.8 x (temp.temp-273.15) + 32}}
 function fetch(currentCity) {
-    $.get({
-        url: 'http://api.openweathermap.org/data/2.5/weather?q=' + currentCity + '&APPID=2c7ca9a42dca75e244be247891827ee1',
+    $.ajax({
+        method:'GET',
+        url: '/weather/'+currentCity+'',
         success: function (data) {
             //update the userData using API data
-            console.log(data);
-            var city={
-                name:data.name,
-                temp: data.main,
-                currentCel:convert(data.main.temp),
-                currentFar:Math.round((data.main.temp-273.15)*1.8+32),
-                minTempC:Math.round(data.main.temp_min-273.15),
-                maxTempC:Math.round(data.main.temp_max-273.15),
-                minTempF:Math.round((data.main.temp_min-273.15)*1.8+32),
-                maxTempF:Math.round((data.main.temp_max-273.15)*1.8+32),
-                description:data.weather,
-                sys:data.sys,  
-                comments:[], 
-                date:currentDate.getDate()
-            }
-            userCities.push(city);
+    
+            userCities.push(data);
+            console.log(userCities);
             _renderCityTemps(userCities);
 
         },
